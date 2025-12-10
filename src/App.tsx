05a599a8a14ad1c830dcd778245d1033eddda938
nainsworth <nja8161995@gmail.com>
2025-12-10 16:04:33 -0600

    Created a generalized page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,46 @@
 function App() {
   return (
-    <h1>RecipeAI</h1>
+    <>
+      <header>
+        <h1>RecipeAI</h1>
+      </header>
+      <main>
+        {/* form */}
+        <form action="">
+          <input
+            type="text"
+            placeholder="e.g. chicken breast, rice, spinach..."
+          />
+          <button>+ Add</button>
+        </form>
+
+        {/* ingredient list */}
+        <div>
+          <h2>Ingredients on hand:</h2>
+          <ul>
+            <li>Chicken</li>
+            <li>Milk</li>
+            <li>Basic Seasoning</li>
+            <li>Broccoli</li>
+            <li>Butter</li>
+          </ul>
+        </div>
+
+        {/* generate button */}
+        <div>
+          <div>
+            <h3>Ready for Recipe</h3>
+            <p>Generate a recipe from you list of ingredients</p>
+          </div>
+          <button>Get a Recipe</button>
+        </div>
+
+        {/* generated recipe */}
+        <div>
+          <h2>Suggested Recipe</h2>
+        </div>
+      </main>
+    </>
   );
 }
 
