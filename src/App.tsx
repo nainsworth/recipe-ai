0ff3564fb7418c201b6aeca6709e2ccda1fa4eaa
nainsworth <nja8161995@gmail.com>
2025-12-10 21:55:07 -0600

    Added Lucid Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Sparkles, Zap } from "lucide-react";
 import IngredientForm from "./components/IngredientForm";
 
 function App() {
@@ -9,16 +10,14 @@ function App() {
 
         {/* ingredient list */}
         <div className="ingredient-list">
-          <div className="flex items-center gap-4 mb-8">
-            <p className="max-[368px]:hidden sm:block h-full">icon</p>
-            <h2 className="text-3xl sm:text-4xl font-bold flex flex-wrap items-center gap-2">
-              Ingredients on hand{" "}
-              <span className="bg-backdrop text-base font-normal rounded-xl py-1 px-3">
-                5 items
-              </span>
-            </h2>
-            {/* <p className="bg-backdrop rounded-4xl py-2 px-4">5 items</p> */}
-          </div>
+          <h2 className="text-3xl sm:text-4xl font-bold flex flex-wrap items-center gap-2 mb-10">
+            {" "}
+            <Zap className="text-emerald-400 max-[360px]:hidden" size={32} />
+            Ingredients on hand{" "}
+            <span className="bg-backdrop text-base font-normal rounded-xl py-1 px-3 ml-1">
+              5 items
+            </span>
+          </h2>
           <ul className="ingredient-list grid grid-cols-1 sm:grid-cols-2 gap-4">
             <li>Chicken</li>
             <li>Milk</li>
@@ -31,8 +30,17 @@ function App() {
         {/* generate button */}
         <div className="bg-backdrop rounded-2xl border-emerald-400/30 p-6 sm:py-8 sm:px-12 flex flex-col sm:flex-row justify-between items-center gap-8 ">
           <div className="self-start">
-            <h3 className="text-2xl sm:text-3xl font-bold mb-2">Ready for Recipe</h3>
-            <p className="text-gray-400 ">Generate a recipe from you list of ingredients</p>
+            <h3 className="text-2xl sm:text-3xl font-bold mb-2 flex gap-2">
+              {" "}
+              <Sparkles
+                className="text-emerald-400 max-[360px]:hidden"
+                size={28}
+              />
+              Ready for Recipe
+            </h3>
+            <p className="text-gray-400 ">
+              Generate a recipe from you list of ingredients
+            </p>
           </div>
           <button className="primary-btn">Get a Recipe</button>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import { Flame } from "lucide-react";
+
 const Header = () => {
   return (
-    <header className="font-bold bg-backdrop border-0 border-b flex justify-center items-center p-5 gap-4">
-      <p className="text-emerald-400">logo</p>
+    <header className="font-bold bg-backdrop border-0 border-b flex justify-center items-center p-5">
+      <Flame className="text-emerald-400" size={48} />
+      
       <h1 className="text-5xl ">Recipe<span className="text-emerald-400">AI</span></h1>
     </header>
   );
