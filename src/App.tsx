6299970a357641750e3a31cb31f638f17aaaf6d3
nainsworth <nja8161995@gmail.com>
2025-12-10 20:31:21 -0600

    Refactor: Placed Ingredient form in a component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
+import IngredientForm from "./components/IngredientForm";
+
 function App() {
   return (
     <>
       <main>
         {/* form */}
-        <form
-          action=""
-          className="bg-backdrop max-full rounded-2xl flex flex-col sm:flex-row justify-center items-center gap-4 p-6 sm:p-8"
-        >
-          <input
-            type="text"
-            placeholder="e.g. chicken breast, rice, spinach..."
-            className="bg-black border border-neutral-800 w-full h-full px-6 py-4 rounded-xl xs:text-base sm:text-xl focus:outline-emerald-400"
-          />
-          <button className="bg-emerald-600 text-black text-xl w-full h-full sm:max-w-40  p-3 rounded-xl cursor-pointer hover:bg-emerald-800 transition-colors">
-            + Add
-          </button>
-        </form>
+        <IngredientForm />
 
         {/* ingredient list */}
         <div>
