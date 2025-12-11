074590c2291f517e22ecf31bd4c8931eeaf4b582
nainsworth <nja8161995@gmail.com>
2025-12-10 20:29:42 -0600

    Style ingredient form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="font-bold bg-neutral-900 backdrop-blur-lg border-b border-neutral-800 flex justify-center items-center p-5 gap-4">
+    <header className="font-bold bg-backdrop flex justify-center items-center p-5 gap-4">
       <p className="text-emerald-400">logo</p>
       <h1 className="text-5xl ">Recipe<span className="text-emerald-400">AI</span></h1>
     </header>
